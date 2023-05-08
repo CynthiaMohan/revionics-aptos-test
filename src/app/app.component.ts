880586c6f5dc7bccb01f3c56822cb04467bbd3f2
Cynthia Mohan <cynthiamohan1@gmail.com>
2023-05-08 19:09:17 -0400

    Tried to implement the 2-way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'revionics_aptos_test';
+  dropdownOptions=['Option 1','Option 2 -Select ME','Option 3','Option 4','Option 5','Option 6','Option 7','Option 8','Option 9','Option 10'];
   selectedOption:string='Select an Option';
 
 
