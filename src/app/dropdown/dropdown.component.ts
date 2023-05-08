8d4e4f35e3c14f90c6d56b9f3e2025983f28ba8e
Cynthia Mohan <cynthiamohan1@gmail.com>
2023-05-08 13:44:02 -0400

    Arrow direction changes with open and close

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -1,10 +1,12 @@
-import { Component,Input,Output,EventEmitter } from '@angular/core';
+import { Component,Input,Output,EventEmitter, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-dropdown',
   templateUrl: './dropdown.component.html',
   styleUrls: ['./dropdown.component.css']
 })
+
+
 export class DropdownComponent {
   @Input() options:string[]=[];
   @Input() selectedOption:string='';
@@ -14,6 +16,16 @@ export class DropdownComponent {
   //onClick open and close the dropdown
   public dropdownOpen(){
     this.dropdownContent=!this.dropdownContent;
+    //change arrow display
+    const arrow:any=document.querySelector('#arrow');
+    if(arrow.classList.contains('fa-angle-down')){
+        arrow.classList.remove('fa-angle-down');
+        arrow.classList.add('fa-angle-up');
+    }
+    else{
+        arrow.classList.remove('fa-angle-up');
+        arrow.classList.add('fa-angle-down')
+    }
   }
 
   public onOptionSelected(option:string):void{
